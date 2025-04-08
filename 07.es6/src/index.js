e6ace915163f18c0f6d5a9ffa4463ced93eec9b3
ANJIYOUNG0109 <jiyoung0109@naver.com>
2025-04-08 18:22:43 +0900

    4/8 8번프로젝트까지 완료

--- 07.es6/src/index.js
+++ 07.es6/src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ArrowFun from './ArrowFunction/ArrowFunction';
+import Assign from './assignment/Assignment';
+import CallbackFun from './callbackFunction/CallbackFun';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <ArrowFun/>
+    <Assign/>
+    <CallbackFun/>
   </React.StrictMode>
 );
 
